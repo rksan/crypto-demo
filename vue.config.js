a06f2da258868bdc6ee7c2308ac7d1d4dc8c3f4f
rk <48819681+rksan@users.noreply.github.com>
2023-05-24 13:27:01 +0900

    add bootstrap & crypto

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
+const webpack = require("webpack");
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        crypto: require.resolve("crypto-browserify"),
+        process: require.resolve("process/browser"),
+        zlib: require.resolve("browserify-zlib"),
+        stream: require.resolve("stream-browserify"),
+        util: require.resolve("util"),
+        buffer: require.resolve("buffer"),
+        asset: require.resolve("assert"),
+      },
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ["buffer", "Buffer"],
+        process: "process/browser",
+      }),
+    ],
+  },
 });
